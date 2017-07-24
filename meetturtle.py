c937eb08b2eeafb248cfbe4ee8998be9e1582297
elias19-meet <elias19@meet.mit.edu>
2017-07-24 12:22:25 +0300

    Stopping here

--- meetturtle.py
+++ meetturtle.py
@@ -1,11 +1,50 @@
 import turtle
-turtle.mainloop()
+#M
 turtle.penup()
 turtle.goto(-200,-100)
 turtle.pendown()
 turtle.goto(-200,-100+200)
 turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+#E n1
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(-50,-100+200)
+turtle.goto(50,-100+200)
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.goto(50,-100)
+turtle.penup()
+turtle.pendown()
+turtle.goto(-50,-100)
+turtle.goto(-50,0)
+turtle.goto(50,0)
+#e n2
+turtle.penup()
+turtle.goto(100,-100)
+turtle.pendown()
+turtle.goto(100,-100+200)
+turtle.goto(200,-100+200)
+turtle.penup()
+turtle.goto(100,-100)
+turtle.goto(200,-100)
+turtle.penup()
+turtle.pendown()
+turtle.goto(100,-100)
+turtle.goto(100,0)
+turtle.goto(200,0)
+#T
+turtle.penup()
+turtle.goto(300,-100)
+turtle.pendown()
+turtle.goto(300,-100+200)
+turtle.goto(250,-100+200)
+turtle.goto(350,-100+200)
 
+
+turtle.mainloop()
 
 
 
